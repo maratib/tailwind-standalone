00426afebd198db26fe3d69da25e4855036dbfb9
Maratib <maratib@gmail.com>
2021-03-02 04:38:11 +0500

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+// @ts-nocheck
+// postcss.config.js
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano')({
+	preset: 'default'
+});
+const purgecss = require('@fullhuman/postcss-purgecss')({
+	content: ['./src/**/*.html', './src/**/*.js'],
+	defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+module.exports = {
+	plugins: [
+		tailwindcss,
+		autoprefixer,
+		...(process.env.NODE_ENV === "production" ? [purgecss, cssnano] : [])
+	]
+};
