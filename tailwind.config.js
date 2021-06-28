88520d9ac1b042c886f72b15d984337d7664c07d
Shaharoz Ali Khan <shahrozdomore@gmail.com>
2021-06-28 20:33:16 +0500

    TailwindCSS version 2.2 added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,63 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 // tailwind.config.js
 module.exports = {
+  mode: 'jit',
   purge: [
     './src/**/*.html',
     './src/**/*.js',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: { fontFamily: {
+        sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+      },
+
+      colors: {
+        "bg-start": "#38738c",
+        "app-bg": "#111827",
+        "search-bg": "#4B5563",
+        "light-yellow": "#FEF3C7",
+        "requester-bg": "#FEF3C7",
+        "yellow-nav": "#E7BA19",
+        "midnightblue": "#192745",
+        "medium-gray": "#4B5563",
+        "normal-gray": "#9CA3AF",
+        "request-text": "#787878",
+        "darkslategray": "#202a3e",
+        "skyblue": "#5FC4EE",
+        current: "#ffffff",
+      },
+      width: {
+        "card-width": "232px",
+        "card-width-xl": "280px",
+        "popular-film-thumbnail": "120px",
+        "popular-film-width": "380px",
+        "73px": "73px",
+        "100px": "100px",
+        "366px": "366px",
+      },
+      height: {
+        "popular-film-thumbnail": "120px",
+        "128px": "128px",
+        "52px": "52px",
+        "73px": "73px",
+        "100px": "100px",
+      },
+      borderRadius: {
+        'large': '10px',
+      }
+    },
   },
+
   variants: {
-    extend: {},
+    extend: {
+      opacity: ["disabled"],
+    },
   },
-  plugins: [],
+
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
